704685d755c1a193273c1096aef141f946a2eb18
Sergey Kozlov <skozlovf@gmail.com>
2019-02-04 11:16:37 +0600

    Rework post hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,17 +1,52 @@
 import os
-import os.path as op
+from pathlib import Path
 
 root_dir = os.path.realpath(os.path.curdir)
-package_dir = op.join(root_dir, 'src', '{{ cookiecutter.package_name }}')
+package_dir = Path(root_dir, 'src', '{{ cookiecutter.package_name }}')
+
+
+class PathsRemover:
+    """Helper to remove added paths."""
+
+    def __init__(self):
+        self.paths = []
+
+    def add_from_root(self, *args):
+        """Add path relative to package root directory.
+
+        Args:
+            args: Path segments.
+        """
+        self.paths.append(Path(root_dir, *args))
+
+    def add_from_package(self, *args):
+        """Add path relative to package source directory.
+
+        Args:
+            args: Path segments.
+        """
+        self.paths.append(Path(package_dir, *args))
+
+    def remove(self):
+        """Remove added paths."""
+        for path in self.paths:
+            if path.is_dir():
+                path.rmdir()
+            else:
+                path.unlink()
 
 
 if __name__ == '__main__':
+    paths = PathsRemover()
+
     if '{{ cookiecutter.use_cli }}' != 'y':
-        os.unlink(op.join(package_dir, 'cli.py'))
-        os.unlink(op.join(package_dir, '__main__.py'))
+        paths.add_from_package('cli.py')
+        paths.add_from_package('__main__.py')
 
     if '{{ cookiecutter.use_bitbucket_pipeline }}' != 'y':
-        os.unlink(op.join(root_dir, 'bitbucket-pipelines.yml'))
+        paths.add_from_root('bitbucket-pipelines.yml')
 
     if '{{ cookiecutter.license }}' == 'No license':
-        os.unlink(op.join(root_dir, 'LICENSE'))
+        paths.add_from_root('LICENSE')
+
+    paths.remove()
