ab7c711713ae5d2e0f46294edde0b3f6b31f3be4
Sergey Kozlov <skozlovf@gmail.com>
2019-09-27 11:42:53 +0600

    Improve configuration loading

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/utils/config.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/utils/config.py
@@ -1,6 +1,57 @@
 from config_source import DictConfig, DictConfigLoader
 
 
+def load_config(sources):
+    """Load configuration from the given sources.
+
+    Args:
+        sources: Source or list of sources to load.
+
+    Returns:
+        :class:`DictConfig` instance.
+    """
+    config = DictConfig(defaults={'pyfile': {'silent': True}})
+    loader = DictConfigLoader(config)
+
+    if isinstance(sources, (tuple, list)):
+        config_sources = sources[:]
+    elif sources is not None:
+        config_sources = [sources]
+    else:
+        config_sources = []
+
+    for src in config_sources:
+        loader.load(src)
+
+    return config
+
+
+def get_namespace(config, namespace, lowercase=True, trim_namespace=True):
+    """Get a subset of options with the given namespace (prefix).
+
+    Args:
+        config: Dict-like object with options.
+        namespace: Configuration namespace.
+        lowercase: Make result options' keys lowercase.
+        trim_namespace: Remove namespace prefix from keys.
+
+    Returns:
+        Dictionary with options.
+    """
+    rv = {}
+    for k, v in config.items():
+        if not k.startswith(namespace):
+            continue
+        if trim_namespace:
+            key = k[len(namespace):]
+        else:
+            key = k
+        if lowercase:
+            key = key.lower()
+        rv[key] = v
+    return rv
+
+
 def create_config(source, env_prefix=None, defaults=None):
     """Create app configuration.
 
@@ -13,17 +64,23 @@ def create_config(source, env_prefix=None, defaults=None):
 
     Add the following fields to local config to load S3 config::
 
-        ...
+        S3CONFIG_BUCKET_NAME = 'mybucket'
+        S3CONFIG_FILENAME = 'path/in/bucket/cfg.py'
+        S3CONFIG_CACHE_FILENAME = '/path/to/cache/file.py'
+        S3CONFIG_UPDATE_CACHE = False
 
-        S3 = dict(
-            bucket_name='mybucket',
-            filename='path/in/bucket/cfg.py',
-            # profile='',
-            # access_key='',
-            # secret_key='',
-            # cache_filename='/path/to/cache/file.py',
-            # update_cache=False
-        )
+        # Skip S3 config loading.
+        # S3CONFIG_SKIP = True
+
+        # AWS credentials.
+        #
+        # NOTE: you may use standard env vars to specify credentials:
+        # AWS_PROFILE, AWS_REGION, AWS_DEFAULT_REGION, AWS_ACCESS_KEY,
+        # AWS_SECRET_KEY.
+        #
+        # S3CONFIG_PROFILE = ''
+        # S3CONFIG_ACCESS_KEY = ''
+        # S3CONFIG_SECRET_KEY = ''
 
     Args:
         source: Single configuration source or list of them (filenames,
@@ -41,34 +98,31 @@ def create_config(source, env_prefix=None, defaults=None):
     See Also:
         https://docs.aws.amazon.com/cli/latest/reference/s3/
     """
-    if isinstance(defaults, (tuple, list)):
-        config_sources = defaults[:]
-    elif defaults is not None:
-        config_sources = [defaults]
-    else:
-        config_sources = []
+    env_config = DictConfig()
+    if env_prefix:
+        env_config.load_from('env', prefix=env_prefix)
 
-    if isinstance(source, (tuple, list)):
-        config_sources.extend(source)
-    elif source:
-        config_sources.append(source)
+    configs = [
+        load_config(defaults),
+        load_config(source),
+        env_config
+    ]
 
-    config = DictConfig(defaults={'pyfile': {'silent': True}})
-    loader = DictConfigLoader(config)
+    s3_config = {}
+    for config in configs:
+        s3 = get_namespace(config, 'S3CONFIG_', lowercase=True)
+        if s3:
+            s3_config.update(s3)
 
-    for src in config_sources:
-        loader.load(src)
-
-    if env_prefix is not None:
-        config.load_from('env', prefix=env_prefix)
-
-    # If S3 field is set then load S3 config
-    # and merge already loaded files/env config into it.
-    s3 = config.get('S3')
-    if s3 is not None and not s3.get('skip', False):
-        s3config = DictConfig(defaults={'s3': s3})
+    # If S3 config exists then load it and put after default.
+    if s3_config and not s3_config.get('skip', False):
+        s3config = DictConfig(defaults={'s3': s3_config})
         s3config.load_from('s3')
-        s3config.load_from('dict', config)
-        config = s3config
+        configs.insert(1, s3config)
+
+    # Merge loaded configs.
+    config = {}
+    for cfg in configs:
+        config.update(cfg)
 
     return config
